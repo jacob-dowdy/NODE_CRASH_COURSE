5a441895e5eef091b12d5245709e3bf080a97f47
Jacob Dowdy <jacobdowdy@rocketmail.com>
2021-10-18 17:38:48 -0400

    add server

--- logger.js
+++ logger.js
@@ -8,4 +8,14 @@ class Logger extends EventEmitter {
   }
 }
 
-module.exports = Logger;
+// module.exports = Logger;
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener', data));
+
+logger.log('Hello World');
+logger.log('Hi');
+logger.log('Hello');
